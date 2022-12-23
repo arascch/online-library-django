6b8c6e1d9f6355a57ffabba4d4c18aac9352bd0a
Arash Askari <arash.ask.94@gmail.com>
2022-12-23 12:10:04 +0330

    create Author models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book, BookInstance, Genre , Author
 
+admin.register(Book)
+admin.register(Author)
+admin.register(Genre)
+admin.register(BookInstance)
 # Register your models here.

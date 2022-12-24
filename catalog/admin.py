d60e7c98db365163cf9378026f2c12ac7e7c7550
Arash Askari <arash.ask.94@gmail.com>
2022-12-24 19:46:14 +0330

    define models in admin file

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Book, BookInstance, Genre , Author
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(Book)
+#admin.site.register(Genre)
+#admin.site.register(BookInstance)
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass

0bbfa54a4bb401853b0c722cf79e342a621fff31
Arash Askari <arash.ask.94@gmail.com>
2023-01-07 22:16:26 +0330

    ,

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from .models import Book , Author , BookInstance , Genre
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #view for home page
 def index(request):
@@ -42,3 +43,10 @@ class BookDetailView(generic.DetailView):
         return render(requset , 'catalog/book_detail.html' , context={'book':book})
 
 
+class LoanedBooksByUserListView(LoginRequiredMixin , generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')

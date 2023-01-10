6c61e8c31cb4ab33819dad6d44740e67e65eca36
Arash Askari <arash.ask.94@gmail.com>
2023-01-10 21:54:49 +0330

    add view for renew form

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from .models import Book , Author , BookInstance , Genre
 from django.views import generic
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404 ,render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+import datetime
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from catalog.forms import RenewBookForm
 
 #view for home page
 def index(request):
@@ -50,3 +53,23 @@ class LoanedBooksByUserListView(LoginRequiredMixin , generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+def renew_book_librarian(request , pk):
+    book_instance = get_object_or_404(BookInstance , pk=pk)
+    if request.method=='POST':
+        form = RenewBookForm(request.POST)
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date':proposed_renewal_date})
+
+    context = {
+        'form':form,
+        'book_instance': book_instance,
+    }
+
+    return render(request , 'catalog/book_renew_librarian.html' , context)
